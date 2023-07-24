033fc3781230bd2a7759b23469f2371ffd8b54a9
031131ym <140357983+031131ym@users.noreply.github.com>
2023-07-24 17:51:37 +0900

    Update app.py
    行動に関する補記。

--- app.py
+++ app.py
@@ -70,7 +70,7 @@ def communicate():
 # ユーザーインターフェイスの構築
 st.title(" 対話型ゲーム")
 st.image("05_rpg.png")
-st.write("中世風RPGです。行動回数が0になる前に魔王を倒してください。行動回数は最大5回です。")
+st.write("中世風RPGです。あなたは勇者です。最大5回行動できます。行動回数が0になる前に魔王を倒してください。必要な行動を入力してください。")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
 
